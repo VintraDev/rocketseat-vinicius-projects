3e46a6da3ec7914d8a304c132acee54991fee03e
vintradev <165097853+VintraDev@users.noreply.github.com>
2026-07-23 15:15:44 -0300

    Chore: project config

--- Next/pet-shop/app/page.tsx
+++ Next/pet-shop/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+type Props = {
+  title: string;
+}
 
 export default function Home() {
+
+  const Component = ({ title }: Props ) => {
+    console.log(title);
+
+    return (
+      <>
+        <h2>Component</h2>
+      </>
+    )
+  }
+
   return (
     <div>
       <h2>RocketSeat</h2>
+      <Component title={2} />
     </div>
   );
 }
