082f5b3164eaf486b7405d2220fca9c77ec516c6
vintradev <165097853+VintraDev@users.noreply.github.com>
2026-08-03 13:36:56 -0300

    Feat: Implements schedule form

--- Next/pet-shop/src/components/appointment-form/appointment-form.tsx
+++ Next/pet-shop/src/components/appointment-form/appointment-form.tsx
@@ -9,21 +9,59 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '../ui/button';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form } from '../ui/form';
+
+const appointmentFormSchema = z.object({
+  tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
+  petName: z.string().min(3, 'O nome do pet é obrigatório'),
+  phone: z.string().min(11, 'O telefone é obrigatório'),
+  description: z.string().min(3, 'A descrição é obrigatório'),
+});
+
+type AppointmentFormValuesProps = z.infer<typeof appointmentFormSchema>;
 
 export function AppointmentForm() {
+  const form = useForm<AppointmentFormValuesProps>({
+    resolver: zodResolver(appointmentFormSchema),
+    defaultValues: {
+      tutorName: '',
+      petName: '',
+      phone: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = (data: AppointmentFormValuesProps) => {
+    console.log(data);
+  };
+
   return (
     <Dialog>
-      <DialogTrigger
-        render={<Button variant="brand">Novo Agendamento</Button>}
-      />
+      <DialogTrigger asChild>
+        <Button variant="brand">Novo Agendamento</Button>
+      </DialogTrigger>
 
-      <DialogContent>
+      <DialogContent
+        variant="appointment"
+        overlayVariant="blurred"
+        showCloseButton
+      >
         <DialogHeader>
-          <DialogTitle>Agende um atendimento</DialogTitle>
-          <DialogDescription>
+          <DialogTitle size="modal">Agende um atendimento</DialogTitle>
+          <DialogDescription size="modal">
             Preencha os dados do cliente para realizar o agendamento:
           </DialogDescription>
         </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <input {...form.register('tutorName')} type="text" />
+            <Button type="submit">Salvar</Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
