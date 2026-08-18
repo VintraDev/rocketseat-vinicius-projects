71b0ea36f14c585b064a1816f5953dd2f6094181
vintradev <165097853+VintraDev@users.noreply.github.com>
2026-08-18 09:52:48 -0300

    chore: Implements initial project config

--- Next/board-app/eslint.config.mjs
+++ Next/board-app/eslint.config.mjs
@@ -1,18 +1,33 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextConfig from 'eslint-config-next';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = [
+  // 1. Configurações base do Next.js
+  ...nextConfig,
+
+  // 2. Integração com Prettier (desativa regras conflitantes e roda o prettier como regra eslint)
+  eslintPluginPrettierRecommended,
+
+  // 3. Ignorar pastas e builds
+  {
+    ignores: [
+      'src/generated/prisma/**',
+      'src/app/generated/prisma/**',
+      'pgdata/**',
+      '.next/**',
+      'node_modules/**',
+    ],
+  },
+
+  // 4. Regras customizadas
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'prettier/prettier': 'error', // Garante que desvios de formatação apontem como erro no linter
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
+];
 
 export default eslintConfig;
