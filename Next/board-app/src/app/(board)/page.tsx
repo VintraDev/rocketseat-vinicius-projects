d194aa5e7f75c1cc45a2f2c6431f6d61a5183509
VintraDev <165097853+VintraDev@users.noreply.github.com>
2026-09-05 21:48:33 -0300

    feat: Implements To-do, in progress and done issues

--- Next/board-app/src/app/(board)/page.tsx
+++ Next/board-app/src/app/(board)/page.tsx
@@ -54,6 +54,114 @@ export default async function Home({ searchParams }: boardProps) {
           })}
         </Section.Content>
       </Section.Root>
+
+      <Section.Root>
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            To-do
+          </Section.Title>
+
+          <Section.IssueCount>{issues.todo.length}</Section.IssueCount>
+        </Section.Header>
+
+        {/* Content */}
+        <Section.Content>
+          {issues.todo.map((issues) => {
+            return (
+              <Card.Root key={issues.id}>
+                <Card.Header>
+                  <Card.Number>{issues.issueNumber}</Card.Number>
+                  <Card.Title>{issues.title}</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer">
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
+
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">6</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            );
+          })}
+        </Section.Content>
+      </Section.Root>
+
+      <Section.Root>
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            In Progress
+          </Section.Title>
+
+          <Section.IssueCount>{issues.in_progress.length}</Section.IssueCount>
+        </Section.Header>
+
+        {/* Content */}
+        <Section.Content>
+          {issues.in_progress.map((issues) => {
+            return (
+              <Card.Root key={issues.id}>
+                <Card.Header>
+                  <Card.Number>{issues.issueNumber}</Card.Number>
+                  <Card.Title>{issues.title}</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer">
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
+
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">6</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            );
+          })}
+        </Section.Content>
+      </Section.Root>
+
+      <Section.Root>
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            Done
+          </Section.Title>
+
+          <Section.IssueCount>{issues.done.length}</Section.IssueCount>
+        </Section.Header>
+
+        {/* Content */}
+        <Section.Content>
+          {issues.done.map((issues) => {
+            return (
+              <Card.Root key={issues.id}>
+                <Card.Header>
+                  <Card.Number>{issues.issueNumber}</Card.Number>
+                  <Card.Title>{issues.title}</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer">
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
+
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">6</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            );
+          })}
+        </Section.Content>
+      </Section.Root>
     </main>
   );
 }
