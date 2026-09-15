bcd5eaa170b13d3a6f600d3f3da2c52dab75eb70
vintradev <165097853+VintraDev@users.noreply.github.com>
2026-09-15 15:02:49 -0300

    feat: Implements loading state on single issue page

--- Next/board-app/src/http/get-issue.ts
+++ Next/board-app/src/http/get-issue.ts
@@ -1,5 +1,6 @@
 import { IssueSchema } from '@/api/routes/get-issue';
 import { clientEnv } from '@/env';
+import { setTimeout } from 'node:timers/promises';
 
 interface GetIssueParams {
   id?: string;
